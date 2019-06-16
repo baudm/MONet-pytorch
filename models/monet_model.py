More numerical stability fixes
Clamp tensor to min=eps prior to log() operations

models/monet_model.py
@@ -51,7 +51,7 @@ class MONetModel(BaseModel):
         self.netCVAE = networks.init_net(networks.ComponentVAE(opt.input_nc, opt.z_dim), gpu_ids=self.gpu_ids)
         # define networks; you can use opt.isTrain to specify different behaviors for training and test.
         if self.isTrain:  # only defined during training time
-            self.criterionRecon = nn.MSELoss(reduction='sum')
+            self.eps = torch.finfo(torch.float).eps
             self.criterionKL = nn.KLDivLoss(reduction='batchmean')
             self.optimizer = optim.RMSprop(chain(self.netAttn.parameters(), self.netCVAE.parameters()), lr=opt.lr)
             self.optimizers = [self.optimizer]
@@ -70,14 +70,14 @@ class MONetModel(BaseModel):
         # Initial s_k = 1: shape = (N, 1, H, W)
         shape = list(self.x.shape)
         shape[1] = 1
-        log_s_k = torch.zeros(tuple(shape), dtype=torch.float, device=self.device)
+        log_s_k = self.x.new_zeros(shape)
 
         self.x_tilde = 0
+        b = []
         m = []
         m_tilde_logits = []
         z_mu = []
         z_logvar = []
-        b = []
 
         for k in range(self.opt.num_slots):
             # Derive mask from current scope
@@ -85,7 +85,7 @@ class MONetModel(BaseModel):
                 log_alpha_k = self.netAttn(self.x, log_s_k)
                 log_m_k = log_s_k + log_alpha_k
                 # Compute next scope
-                log_s_k += (1. - log_alpha_k.exp()).log()
+                log_s_k += (1. - log_alpha_k.exp()).clamp(min=self.eps).log()
             else:
                 log_m_k = log_s_k
 
@@ -113,7 +113,7 @@ class MONetModel(BaseModel):
             z_mu.append(z_mu_k)
             z_logvar.append(z_logvar_k)
 
-        self.b = torch.cat(b, 1)
+        self.b = torch.cat(b, dim=1)
         self.m = torch.cat(m, dim=1)
         self.m_tilde_logits = torch.cat(m_tilde_logits, dim=1)
         self.z_mu = torch.cat(z_mu, dim=1)
@@ -121,8 +121,9 @@ class MONetModel(BaseModel):
 
     def backward(self):
         """Calculate losses, gradients, and update network weights; called in every training iteration"""
-        self.loss_E = -0.5 * (1 + self.z_logvar - self.z_mu.pow(2) - self.z_logvar.exp()).sum(dim=-1).mean()
-        self.loss_D = -torch.logsumexp(self.m.log().unsqueeze(2) + self.b, dim=1).sum() / self.x.shape[0]
+        n = self.x.shape[0]
+        self.loss_E = -0.5 * (1 + self.z_logvar - self.z_mu.pow(2) - self.z_logvar.exp()).sum() / n
+        self.loss_D = -torch.logsumexp(self.m.clamp(min=self.eps).log().unsqueeze(2) + self.b, dim=1).sum() / n
         self.loss_mask = self.criterionKL(self.m_tilde_logits.log_softmax(dim=1), self.m)
         loss = self.loss_D + self.opt.beta * self.loss_E + self.opt.gamma * self.loss_mask
         loss.backward()